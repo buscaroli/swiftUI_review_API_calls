db346b2d86a965d457f688b6a4d14e51df2da828
buscaroli <m.buscaroli@outlook.com>
2023-01-02 17:42:30 +0000

    Initial Commit

---
+++ CallingAPI/CallingAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallingAPIApp.swift
+//  CallingAPI
+//
+//  Created by Matteo on 02/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CallingAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
